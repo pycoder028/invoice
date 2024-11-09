f601d7ab0bd77288d530740ca13fc182728ffc9a
Riyad <pycoder028@gmail.com>
2024-11-09 19:47:50 +0600

    Install vuejs and Connect with Laravel

---
+++ resources/js/components/NotFound.vue
@@ -0,0 +1,5 @@
+<template>
+    <h1 style="color: red;">Page not found</h1>
+    <br><hr>
+    <router-link to="/">Back to Home Page</router-link>
+</template>
---
+++ resources/js/components/invoices/index.vue
@@ -0,0 +1,3 @@
+<template>
+    <h2>Index invoice page component</h2>
+</template>
